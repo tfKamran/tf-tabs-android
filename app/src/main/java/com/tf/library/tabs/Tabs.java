55d38454468859dadc17bbad52b861833cc9be7d
Kamran <team.radiant@gmail.com>
2014-11-27 09:21:44 +0530

    Initial commit

---
+++ app/src/main/java/com/tf/library/tabs/Tabs.java
@@ -0,0 +1,131 @@
+package com.tf.library.tabs;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.tf.library.tabs.example.R;
+
+import java.util.ArrayList;
+
+public class Tabs extends LinearLayout {
+    private Context context;
+    private ArrayList<Tab> tabs;
+    private int backgroundColor;
+    private int selectionColor;
+
+    public Tabs(Context context) {
+        super(context);
+
+        initialize(context);
+    }
+
+    public Tabs(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        initialize(context);
+    }
+
+    private void initialize(Context context) {
+        setOrientation(LinearLayout.HORIZONTAL);
+
+        this.context = context;
+    }
+
+    public void setViewPager(ViewPager pager) {
+        int count = pager.getAdapter().getCount();
+        tabs = new ArrayList<Tab>(count);
+
+        for (int position = 0; position < count; position++) {
+            Tab tab = new Tab(context, position, pager);
+            tab.setSelected(position == 0);
+
+            tabs.add(tab);
+            addView(tab.getView());
+
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) tab.getView().getLayoutParams();
+            layoutParams.weight = 1;
+            tab.getView().setLayoutParams(layoutParams);
+        }
+    }
+
+    public void setCurrentTab(int position) {
+        int count = tabs.size();
+        for (int index = 0; index < count; index++)
+            tabs.get(index).setSelected(index == position);
+    }
+
+    public void setBackgroundColor(int color) {
+        backgroundColor = color;
+
+        for (Tab tab : tabs)
+            tab.setBackgroundColor(color);
+    }
+
+    public void setTitleColor(int color) {
+        backgroundColor = color;
+
+        for (Tab tab : tabs)
+            tab.setTitleColor(color);
+    }
+
+    public void setSelectionColor(int color) {
+        selectionColor = color;
+
+        for (Tab tab : tabs)
+            tab.setSelectionColor(color);
+    }
+
+    private class Tab {
+        private View tabView;
+
+        public Tab(Context context, final int position, final ViewPager pager) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            tabView = layoutInflater.inflate(R.layout.tab, null);
+
+            setTitle(pager.getAdapter().getPageTitle(position));
+
+            tabView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    pager.setCurrentItem(position);
+                }
+            });
+        }
+
+        public View getView() {
+            return tabView;
+        }
+
+        public void setSelected(boolean selected) {
+            if (selected)
+                tabView.findViewById(R.id.selection).setVisibility(View.VISIBLE);
+            else
+                tabView.findViewById(R.id.selection).setVisibility(View.GONE);
+        }
+
+        public CharSequence getTitle() {
+            return ((TextView) tabView.findViewById(R.id.lblTitle)).getText();
+        }
+
+        public void setTitle(CharSequence title) {
+            ((TextView) tabView.findViewById(R.id.lblTitle)).setText(title);
+        }
+
+        public void setBackgroundColor(int color) {
+            tabView.setBackgroundColor(color);
+        }
+
+        public void setTitleColor(int color) {
+            ((TextView) tabView.findViewById(R.id.lblTitle)).setTextColor(color);
+        }
+
+        public void setSelectionColor(int color) {
+            tabView.findViewById(R.id.selection).setBackgroundColor(color);
+        }
+    }
+}
---
+++ app/src/main/java/com/tf/library/tabs/example/ExampleActivity.java
@@ -0,0 +1,142 @@
+package com.tf.library.tabs.example;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tf.library.tabs.Tabs;
+
+import java.util.Locale;
+
+
+public class ExampleActivity extends ActionBarActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+
+        // Set up the action bar.
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // Set up the tabs
+        final Tabs tabs = (Tabs) findViewById(R.id.tabs);
+        tabs.setViewPager(mViewPager);
+        tabs.setBackgroundColor(getResources().getColor(R.color.primary_material_light));
+        tabs.setTitleColor(getResources().getColor(android.R.color.black));
+        tabs.setSelectionColor(getResources().getColor(R.color.primary_material_dark));
+
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                tabs.setCurrentTab(position);
+            }
+        });
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_example, container, false);
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_section1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section3).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+
+}
